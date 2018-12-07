683144438714ea9890d7083933ed5233cecd15d9
r3drum <mauriciomorales0410@gmail.com>
2018-12-07 15:50:56 -0300

    modificaciones

---
+++ database/migrations/2018_11_26_181749_add_foreign_keys_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddForeignKeysToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+          $table->foreign('rol_id')->references('id')->on('rols');
+          $table->foreign('cargo_id')->references('id')->on('cargos');
+          $table->foreign('unidad_id')->references('id')->on('unidads');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
