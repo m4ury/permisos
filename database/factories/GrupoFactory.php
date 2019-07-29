10be297725a60b1863fbb4a3055db44c12cc262b
r3drum <mauriciomorales0410@gmail.com>
2019-07-29 08:58:24 -0400

    xd

---
+++ database/factories/GrupoFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Grupo;
+use Faker\Generator as Faker;
+
+$factory->define(Grupo::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
